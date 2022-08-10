34610404991691eb8061a757ea96a5bfff7845e8
Kolpa4 <kolpa4.vhla@mail.ru>
2022-08-10 20:43:33 +0000

    The square is MOVING Holy Shit its alive

---
+++ portal/src/App.js
@@ -0,0 +1,27 @@
+import React,{useState} from 'react'
+
+
+function App() {
+  const [coordinates, setCoordinates] = useState({x:0, y:0})
+  
+  
+  console.log(coordinates)
+
+  return (
+    <div className="App">
+      <div style ={{
+        display:'flex',
+        position:'absolute',
+        width: '100px',
+        height: '100px',
+        top: coordinates.y,
+        left: coordinates.x,
+        backgroundColor: 'black'
+        }}
+        onMouseDown={(e) => setCoordinates({x:e.pageX, y:e.pageY})}
+      />
+    </div>
+  );
+}
+
+export default App;
